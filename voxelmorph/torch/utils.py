c6ec8fe66338919c950b3fe04abd0e2018bbdbac
farid <mf.azampour@tum.de>
2020-12-03 09:03:51 +0100

    showing logsimga

--- voxelmorph/torch/utils.py
+++ voxelmorph/torch/utils.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
@@ -10,7 +12,8 @@ def set_axs_attribute(axs):
         ax.axes.yaxis.set_visible(False)
 
 
-def fill_subplots(img: torch.Tensor, axs, img_name='', fontsize=6, cmap='gray'):
+def fill_subplots(img: torch.Tensor, axs, img_name='', fontsize=6, cmap='gray',
+                  fig: plt.Figure=None, show_colorbar=False):
     if cmap == 'gray':  # map image to 0...1
         img = (img + img.min())/(img.max() - img.min())
     elif cmap is None:  # cliping data to 0...255
@@ -18,16 +21,26 @@ def fill_subplots(img: torch.Tensor, axs, img_name='', fontsize=6, cmap='gray'):
         img[img > 255] = 255
 
     shape = img.shape[-3:]
-    axs[0].imshow(img[0, :, int(shape[0] / 2), :, :].permute(dims=(1, 2, 0)).squeeze().numpy(), cmap=cmap)
+    img1 = img[0, :, int(shape[0] / 2), :, :].permute(dims=(1, 2, 0)).squeeze().numpy()
+    axs[0].imshow(img1, cmap=cmap)
     axs[0].set_title(f'{img_name} central slice \n in sagittal view', fontsize=fontsize)
-    axs[1].imshow(img[0, :, :, int(shape[1] / 2), :].permute(dims=(1, 2, 0)).squeeze().numpy(), cmap=cmap)
+    img1 = img[0, :, :, int(shape[1] / 2), :].permute(dims=(1, 2, 0)).squeeze().numpy()
+    axs[1].imshow(img1, cmap=cmap)
     axs[1].set_title(f'{img_name} central slice \n in coronal view', fontsize=fontsize)
-    axs[2].imshow(img[0, :, :, :, int(shape[0] / 2)].permute(dims=(1, 2, 0)).squeeze().numpy(), cmap=cmap)
+    img2 = img[0, :, :, :, int(shape[0] / 2)].permute(dims=(1, 2, 0)).squeeze().numpy()
+    axs[2].imshow(img2, cmap=cmap)
     axs[2].set_title(f'{img_name} central slice \n in axial view', fontsize=fontsize)
+    if show_colorbar and fig is not None:
+        fig.colorbar(img1, ax=axs[0])
+        fig.colorbar(img1, ax=axs[1])
+        fig.colorbar(img2, ax=axs[2])
 
-
-def create_figure(fixed: torch.Tensor, moving: torch.Tensor, registered: torch.Tensor, deformation: torch.Tensor):
-    nrow = 6
+def create_figure(fixed: torch.Tensor, moving: torch.Tensor, registered: torch.Tensor,
+                  deformation: torch.Tensor, logSimga: None):
+    if logSimga is None:
+        nrow = 6
+    else:
+        nrow = 7
     ncol = 3
     axs, fig = init_figure(ncol, nrow)
 
@@ -40,10 +53,12 @@ def create_figure(fixed: torch.Tensor, moving: torch.Tensor, registered: torch.T
     fill_subplots(fixed - registered, axs=axs[4, :], img_name='Fix-Reg')
     deform_ = (deformation + 5) / 10
     fill_subplots(deform_, axs=axs[5, :], img_name='Def.', cmap=None)
+    if logSimga:
+        fill_subplots(logSimga, axs=axs[6: 0], img_name='LogSigma', cmap=None, fig=fig, show_colorbar=True)
     return fig
 
 
-def init_figure(ncol, nrow):
+def init_figure(ncol, nrow) -> (List[plt.Axes], plt.Figure):
     fig = plt.figure(figsize=(2 * ncol + 1, 2 * nrow + 1))  # , constrained_layout=True)
     spec = gridspec.GridSpec(nrow, ncol, figure=fig,
                              wspace=0.2, hspace=0.2,
